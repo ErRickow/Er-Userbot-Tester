c7b1caf37f444d8a74270f7343303cfe315683d9
Just R <remixingmagelang@gmail.com>
2024-08-18 02:29:08 +0700

    update plugins/update.py

---
+++ plugins/update.py
@@ -0,0 +1,78 @@
+import re
+import asyncio
+import os
+import sys
+import shutil
+import subprocess
+
+from git import Repo
+from git.exc import InvalidGitRepositoryError
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from utils.misc import plugins_help, prefix, requirements_list
+from utils.db import db
+from utils.anu import format_exc, restart
+
+
+def check_command(command):
+    return shutil.which(command) is not None
+
+
+@Client.on_message(filters.command("restart", prefix) & filters.me)
+async def restart_cmd(_, message: Message):
+    db.set(
+        "core.updater",
+        "restart_info",
+        {
+            "type": "restart",
+            "chat_id": message.chat.id,
+            "message_id": message.id,
+        },
+    )
+
+    if "LAVHOST" in os.environ:
+        await message.edit("<b>Your lavHost is restarting...</b>")
+        os.system("lavhost restart")
+        return
+
+    await message.reply("<blockquote>Sedang Merestart...</blockquote>")
+    restart()
+
+@Client.on_message(filters.command("update", prefix) & filters.me)
+async def update(_, message: Message):
+    db.set(
+        "core.updater",
+        "restart_info",
+        {
+            "type": "update",
+            "chat_id": message.chat.id,
+            "message_id": message.id,
+        },
+    )
+
+    if "LAVHOST" in os.environ:
+        await message.edit("<b>Your lavHost is updating...</b>")
+        os.system("lavhost update")
+        return
+
+    await message.reply("<blockquote>Wet...</blockquote>\n<blockquote>Sedang Update!</blockquote>")
+    try:
+        subprocess.run([sys.executable, "-m", "pip", "install", "-U", "pip"])
+        subprocess.run(["git", "pull"])
+    except Exception as e:
+        await message.reply(format_exc(e))
+        db.remove("core.updater", "restart_info")
+    else:
+        await message.reply("<blockquote>Update: selesai sayang!</blockquote>\n\n<blockquote>Merestart...</blockquote>")
+        restart()
+        await message.reply("<b>_Sudah Selesai Restartnya sayang_...</b>")
+
+#nganu
+
+
+plugins_help["updater"] = {
+    "update": "Update Userbot Lu. Jika ada update an.",
+    "restart": "Restart Userbotnya",
+}
