1a7c4fbbd4e0afb40208d0babfbbee8205927d57
Just R <remixingmagelang@gmail.com>
2024-08-17 18:16:35 +0700

    update plugins/ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -5,3 +5,15 @@ from pyrogram.types import Message
 
 from utils.anu import time_formatter
 from utils.misc import plugins_help, prefix
+
+@Client.on_message(filters.command(["ping", "p"], prefix) & filters.me)
+async def get_readable_time(_, message: Message):
+    start = time.time()
+    nganu = time.time() - start
+    uptime = time_formatter(time.time())
+    await message.reply(f"<blockquote>❏ POMG!!🏓 {nganu * 1000:.3f}ms\n├• Uptime {uptime}</blockquote>")
+
+
+plugins_help["ping"] = {
+    "ping": "Check ping to Telegram servers",
+}
