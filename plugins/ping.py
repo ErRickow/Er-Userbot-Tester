d0a80b56a8117566c47ab2a88cdadf50bcf113bc
Just R <remixingmagelang@gmail.com>
2024-08-17 18:15:57 +0700

    update plugins/ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -4,4 +4,4 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from utils.anu import time_formatter
-from utils.misc import modules_help, prefix
+from utils.misc import plugins_help, prefix
