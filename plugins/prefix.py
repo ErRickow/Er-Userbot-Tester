3972f909e4a8e32d483b1b48b405703b9b3d657f
Just R <remixingmagelang@gmail.com>
2024-08-18 02:37:25 +0700

    update plugins/prefix.py

---
+++ plugins/prefix.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from utils.db import db
+from utils.misc import plugins_help, prefix
+from utils.anu import restart
+
+
+@Client.on_message(
+    filters.command(["sp", "setprefix", "setprefix_Moon"], prefix) & filters.me
+)
+async def setprefix(_, message: Message):
+    if len(message.command) > 1:
+        pref = message.command[1]
+        db.set("core.main", "prefix", pref)
+        await message.reply(f"<blockquote>Prefix [ <code>{pref}</code> ] telah di set!</blockquote>")
+        restart()
+    else:
+        await message.reply("<blockquote>Prefix gaboleh kosong kontol!</blockquote>")
+
+
+plugins_help["prefix"] = {
+    "setprefix [prefix]": "Set custom prefix",
+    "setprefix_Moon [prefix]": "Set custom prefix",
+}
