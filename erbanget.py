1943f7f156eba0f85cf1deeeab76f5fcb2a5f996
Just R <remixingmagelang@gmail.com>
2024-08-18 03:23:35 +0700

    update erbanget.py

--- erbanget.py
+++ erbanget.py
@@ -87,7 +87,7 @@ async def erbanget():
             "update": "<blockquote>Proses Update Sukses Sayangku!</blockquote>",
         }[info["type"]]
         try:
-            await app.message.reply(
+            await app.on_message.reply(
                 info["chat_id"], info["message_id"], text
             )
         except errors.RPCError:
