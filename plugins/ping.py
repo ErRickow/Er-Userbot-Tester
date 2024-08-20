5f87e3c643a17418e6b3726b8d84cab64d36de49
Just R <remixingmagelang@gmail.com>
2024-08-20 23:13:43 +0700

    update plugins/ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -8,6 +8,8 @@ from pyrogram.types import Message
 from utils.anu import time_formatter
 from utils.misc import plugins_help, prefix
 
+StartTime = time.time()
+
 def get_readable_time(seconds: int) -> str:
     count = 0
     ping_time = ""
