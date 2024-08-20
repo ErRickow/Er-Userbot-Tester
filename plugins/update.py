5aea423a5324c4d22be66af6a88dcdc270f0d95c
Just R <remixingmagelang@gmail.com>
2024-08-20 22:54:30 +0700

    update plugins/update.py

--- plugins/update.py
+++ plugins/update.py
@@ -57,7 +57,7 @@ async def update(_, message: Message):
         os.system("lavhost update")
         return
       
-      await message.reply("<blockquote>Wet...<blockquote>\n<blockquote>Sedang Update!</blockquote>")
+      await message.reply("<blockquote>Wet...<blockquote>")
     try:
         subprocess.run([sys.executable, "-m", "pip", "install", "-U", "pip"])
         subprocess.run(["git", "pull"])
