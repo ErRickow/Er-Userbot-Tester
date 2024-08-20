98a68927efca180d545b85d1c95c7e6a8ceda945
Just R <remixingmagelang@gmail.com>
2024-08-20 23:27:51 +0700

    update plugins/up.py

--- plugins/up.py
+++ plugins/up.py
@@ -20,7 +20,7 @@ def check_command(command):
     return shutil.which(command) is not None
 
 
-
+@Client.on_message(filters.command("up", prefix) & filters.me)
 async def ngapdate(client, message):
     pros = await message.reply(
         f"<blockquote> <b>Memeriksa pembaruan resources {bot.me.mention} ..</b></blockquote>"
