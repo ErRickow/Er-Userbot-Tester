368739b56c5e3e94068e3090177192ecc37efffa
Just R <remixingmagelang@gmail.com>
2024-08-21 05:15:19 +0700

    update plugins/evl.py

--- plugins/evl.py
+++ plugins/evl.py
@@ -37,7 +37,7 @@ async def evaluate_handler(_, message: Message):
         stdout, stderr, exc = None, None, None
 
         try:
-            await message.aexec(cmd)
+            await message.send_message(cmd)
         except Exception:
             exc = traceback.format_exc()
 
