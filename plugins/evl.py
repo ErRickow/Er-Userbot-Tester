36f75d5e393f85279ca55355ce3ae67e20f81846
Just R <remixingmagelang@gmail.com>
2024-08-21 04:36:30 +0700

    update plugins/evl.py

--- plugins/evl.py
+++ plugins/evl.py
@@ -27,8 +27,8 @@ async def evaluate_handler(_, m: Message):
             )
             return
         #text = m.sudo_message.text if getattr(m, "sudo_message", None) else m.text
-       cmd = text.split(None, 1)[1]
-       stdout = StringIO
+         cmd = text.split(None, 1)[1]
+         stdout = StringIO
        #cmd = message.text.split(maxsplit=1)[1]
 
         #msg = await message.send_edit("Executing . . .", text_type=["mono"])
