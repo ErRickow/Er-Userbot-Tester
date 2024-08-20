dbc11891ba38a1bea0b1645d59f573ab40012b61
Just R <remixingmagelang@gmail.com>
2024-08-21 03:05:30 +0700

    update plugins/ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -40,7 +40,7 @@ async def anu(client, message):
     start = time.time()
     uptime = get_readable_time((time.time() - StartTime))
     nganu = time.time() - start
-    await message.reply(f"<blockquote>❏ POMG!!🏓 {nganu * 1000:.3f}ms\n├• Uptime <code>{uptime}</code></blockquote>Owner")
+    await message.reply(f"<blockquote>❏ POMG!!🏓 {nganu * 1000:.3f}ms\n├• Uptime <code>{uptime}</code></blockquote>")
 
 
 plugins_help["ping"] = {
