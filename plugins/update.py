8a77689485e2152718e54b69cee55bd122bdaa3f
Just R <remixingmagelang@gmail.com>
2024-08-20 22:52:03 +0700

    update plugins/update.py

--- plugins/update.py
+++ plugins/update.py
@@ -56,6 +56,7 @@ async def update(_, message: Message):
         await message.edit("<b>Your lavHost is updating...</b>")
         os.system("lavhost update")
         return
+      
       await message.reply("<blockquote>Wet...<blockquote>\n<blockquote>Sedang Update!</blockquote>")
     try:
         subprocess.run([sys.executable, "-m", "pip", "install", "-U", "pip"])
