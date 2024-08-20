bd6cbbb6a4beb8cb4ac715acec2b663479d035af
Just R <remixingmagelang@gmail.com>
2024-08-21 02:22:29 +0700

    update plugins/alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -42,7 +42,7 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-@Client.on_message(filters.command("aliive", prefix) & filters.me)
+@Client.on_message(filters.command("alive", prefix) & filters.me)
 async def alive(client, message):
     start_time = time.time()
     uptime = get_readable_time((time.time() - StartTime))
