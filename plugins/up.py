af35637eddae0159a4a6853fe53dafa4efc3fd6f
Just R <remixingmagelang@gmail.com>
2024-08-21 02:06:10 +0700

    update plugins/up.py

--- plugins/up.py
+++ plugins/up.py
@@ -44,7 +44,6 @@ async def ngapdate(client, message):
           caption=f"{X}",
           reply_to_message_id=message.id,
           )
-  await anuk.delete()
   os.remove("output.txt")
   format_line = [f"┣ {line}" for line in out.splitlines()]
   if format_line:
