6cf321eb94b26c592fdf661ea45eb91e9915d91d
Just R <remixingmagelang@gmail.com>
2024-08-21 09:18:09 +0700

    update plugins/ah.py

--- plugins/ah.py
+++ plugins/ah.py
@@ -2,7 +2,7 @@ from prettytable import PrettyTable
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 
-from utils.misc import plugins_help, prefix,
+from utils.misc import plugins_help, prefix, tolong_anu
 from utils.anu import edit_or_reply, split_list
 
 @Client.on_message(filters.command(["tolong", "t"], prefix) & filters.me)
@@ -18,7 +18,7 @@ async def module_help(client: Client, message: Message):
         ac.header = False
         ac.title = "PyroMan-UserBot Modules"
         ac.align = "l"
-        for x in split_list(sorted(CMD_HELP.keys()), 2):
+        for x in split_list(sorted(tolong_anu.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
             message, f"```{str(ac)}```\n• @Lunatic0de × @SharingUserbot •"
@@ -28,8 +28,8 @@ async def module_help(client: Client, message: Message):
         )
 
     if help_arg:
-        if help_arg in CMD_HELP:
-            commands: dict = CMD_HELP[help_arg]
+        if help_arg in tolong_anu:
+            commands: dict = tolong_anu[help_arg]
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `{CMD_HANDLER}{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
@@ -45,8 +45,8 @@ async def module_help(client: Client, message: Message):
 
 
 def add_command_help(module_name, commands):
-    if module_name in CMD_HELP.keys():
-        command_dict = CMD_HELP[module_name]
+    if module_name in tolong_anu.keys():
+        command_dict = tolong_anu[module_name]
     else:
         command_dict = {}
 
@@ -55,4 +55,4 @@ def add_command_help(module_name, commands):
             if y is not x:
                 command_dict[x[0]] = x[1]
 
-    CMD_HELP[module_name] = command_dict
+    tolong_anu[module_name] = command_dict
