e33cebc15eb027d59facef6a69c0deb501935fe9
Just R <remixingmagelang@gmail.com>
2024-08-23 01:21:45 +0700

    update plugins/eval2.py

--- plugins/eval2.py
+++ plugins/eval2.py
@@ -38,11 +38,11 @@ def _parse_eval(value=None):
 #eor = edit_or_reply
 
 @Client.on_message(filters.command(["v", "pyth"], prefix) & filters.me)
-async def tai(event):
+async def tai(_, m: Message):
     try:
-        cmd = event.text.split(maxsplit=1)[1]
+        cmd = m.text.split(maxsplit=1)[1]
     except IndexError:
-        return await event.eor("ha?", time=5)
+        return await m.eor("ha?", time=5)
     xx = None
     mode = ""
     spli = cmd.split()
@@ -51,16 +51,16 @@ async def tai(event):
     #    try:
    #         cm = cmd.split(maxsplit=1)[1]
     #    except IndexError:
-     #       await event.eor("->> Wrong Format <<-")
+     #       await m.eor("->> Wrong Format <<-")
        #     cm = None
        # return cm
 
     if spli[0] in ["-s", "--silent"]:
-        await event.delete()
+        await m.delete()
         mode = "silent"
     elif spli[0] in ["-n", "-noedit"]:
         mode = "no-edit"
-        xx = await event.reply(get_string("com_1"))
+        xx = await m.reply(get_string("com_1"))
     elif spli[0] in ["-gs", "--source"]:
         mode = "gsource"
     elif spli[0] in ["-ga", "--args"]:
@@ -70,22 +70,22 @@ async def tai(event):
     if not cmd:
         return
     if not mode == "silent" and not xx:
-        xx = await event.eor('sabar anjing!')
+        xx = await m.eor('sabar anjing!')
     if black:
         try:
             cmd = black.format_str(cmd, mode=black.Mode())
         except BaseException:
             # Consider it as Code Error, and move on to be shown ahead.
             pass
-    reply_to_id = event
+    reply_to_id = m
     if any(item in cmd for item in KEEP_SAFE().All) and (
-        not (event.out or event.sender_id == eruser_bot.uid)
+        not (m.out or m.sender_id == eruser_bot.uid)
     ):
-        #warning = await event.forward_to(udB.get_key("LOG_CHANNEL"))
-        await event.reply(
-            f"Malicious Activities suspected by {(await event.get_sender())}"
+        #warning = await m.forward_to(udB.get_key("LOG_CHANNEL"))
+        await m.reply(
+            f"Malicious Activities suspected by {(await m.get_sender())}"
             )
-        ignore_eval.append(event.sender_id)
+        ignore_eval.append(m.sender_id)
         return await xx.edit(
             "`Malicious Activities suspected⚠️!\nReported to owner. Aborted this request!`"
         )
@@ -96,7 +96,7 @@ async def tai(event):
     stdout, stderr, exc, timeg = None, None, None, None
     tima = time.time()
     try:
-        value = await aexec(cmd, event)
+        value = await aexec(cmd, m)
     except Exception:
         value = None
         exc = traceback.format_exc()
@@ -120,16 +120,16 @@ async def tai(event):
     evaluation = exc or stderr or stdout or _parse_eval(value)
     if mode == "silent":
         if exc:
-            msg = f"• <b>EVAL ERROR\n\n• CHAT:</b> <code>(event.chat)</code> [<code>{event.chat_id}</code>]"
+            msg = f"• <b>EVAL ERROR\n\n• CHAT:</b> <code>(m.chat)</code> [<code>{m.chat_id}</code>]"
             msg += f"\n\n∆ <b>CODE:</b>\n<code>{cmd}</code>\n\n∆ <b>ERROR:</b>\n<code>{exc}</code>"
-            log_chat = event.reply
+            log_chat = m.reply
             if len(msg) > 4000:
                 with BytesIO(msg.encode()) as out_file:
                     out_file.name = "Eval-Error.txt"
-                return await event.client.send_message(
+                return await m.client.send_message(
                     log_chat, f"`{cmd}`", file=out_file
                 )
-            await event.client.send_message(log_chat, msg, parse_mode="html")
+            await m.client.send_message(log_chat, msg, parse_mode="html")
         return
     tmt = tima * 1000
     timef = time_formatter(tmt)
@@ -143,14 +143,14 @@ async def tai(event):
         final_output = evaluation
         with BytesIO(str.encode(final_output)) as out_file:
             out_file.name = "eval.txt"
-            await event.client.send_file(
-                event.chat_id,
+            await m.client.send_file(
+                m.chat_id,
                 out_file,
                 force_document=True,
                 #thumb=ULTConfig.thumb,
                 allow_cache=False,
                 caption=f"```{cmd}```" if len(cmd) < 998 else None,
-                reply_to=event
+                reply_to=m
             )
         return await xx.delete()
     await xx.reply(final_output)
