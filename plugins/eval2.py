b231a3b996cc8e8e39eb6efe0cfe0f44b9e97f09
Just R <remixingmagelang@gmail.com>
2024-08-23 00:40:05 +0700

    update plugins/eval2.py

--- plugins/eval2.py
+++ plugins/eval2.py
@@ -30,7 +30,7 @@ async def _(event):
     try:
         cmd = event.text.split(maxsplit=1)[1]
     except IndexError:
-        return await event.eor(get_string("devs_2"), time=5)
+        return await event.eor("ha?", time=5)
     xx = None
     mode = ""
     spli = cmd.split()
