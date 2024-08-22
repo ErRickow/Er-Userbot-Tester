adac18334a64869e88654a07c533623b86abe225
Just R <remixingmagelang@gmail.com>
2024-08-23 00:27:15 +0700

    update plugins/eval2.py

---
+++ plugins/eval2.py
@@ -0,0 +1,174 @@
+def _parse_eval(value=None):
+    if not value:
+        return value
+    if hasattr(value, "stringify"):
+        try:
+            return value.stringify()
+        except TypeError:
+            pass
+    elif isinstance(value, dict):
+        try:
+            return json_parser(value, indent=1)
+        except BaseException:
+            pass
+    elif isinstance(value, list):
+        newlist = "["
+        for index, child in enumerate(value):
+            newlist += "\n  " + str(_parse_eval(child))
+            if index < len(value) - 1:
+                newlist += ","
+        newlist += "\n]"
+        return newlist
+    return str(value)
+    
+eor = edit_or_reply
+
+@ultroid_cmd(pattern="eval", fullsudo=True, only_devs=True)
+async def _(event):
+    try:
+        cmd = event.text.split(maxsplit=1)[1]
+    except IndexError:
+        return await event.eor(get_string("devs_2"), time=5)
+    xx = None
+    mode = ""
+    spli = cmd.split()
+
+    async def get_():
+        try:
+            cm = cmd.split(maxsplit=1)[1]
+        except IndexError:
+            await event.eor("->> Wrong Format <<-")
+            cm = None
+        return cm
+
+    if spli[0] in ["-s", "--silent"]:
+        await event.delete()
+        mode = "silent"
+    elif spli[0] in ["-n", "-noedit"]:
+        mode = "no-edit"
+        xx = await event.reply(get_string("com_1"))
+    elif spli[0] in ["-gs", "--source"]:
+        mode = "gsource"
+    elif spli[0] in ["-ga", "--args"]:
+        mode = "g-args"
+    if mode:
+        cmd = await get_()
+    if not cmd:
+        return
+    if not mode == "silent" and not xx:
+        xx = await event.eor(get_string("com_1"))
+    if black:
+        try:
+            cmd = black.format_str(cmd, mode=black.Mode())
+        except BaseException:
+            # Consider it as Code Error, and move on to be shown ahead.
+            pass
+    reply_to_id = event.reply_to_msg_id or event
+    if any(item in cmd for item in KEEP_SAFE().All) and (
+        not (event.out or event.sender_id == ultroid_bot.uid)
+    ):
+        warning = await event.forward_to(udB.get_key("LOG_CHANNEL"))
+        await warning.reply(
+            f"Malicious Activities suspected by {inline_mention(await event.get_sender())}"
+        )
+        _ignore_eval.append(event.sender_id)
+        return await xx.edit(
+            "`Malicious Activities suspected⚠️!\nReported to owner. Aborted this request!`"
+        )
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = StringIO()
+    redirected_error = sys.stderr = StringIO()
+    stdout, stderr, exc, timeg = None, None, None, None
+    tima = time.time()
+    try:
+        value = await aexec(cmd, event)
+    except Exception:
+        value = None
+        exc = traceback.format_exc()
+    tima = time.time() - tima
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    if value:
+        try:
+            if mode == "gsource":
+                exc = inspect.getsource(value)
+            elif mode == "g-args":
+                args = inspect.signature(value).parameters.values()
+                name = ""
+                if hasattr(value, "__name__"):
+                    name = value.__name__
+                exc = f"**{name}**\n\n" + "\n ".join([str(arg) for arg in args])
+        except Exception:
+            exc = traceback.format_exc()
+    evaluation = exc or stderr or stdout or _parse_eval(value) or get_string("instu_4")
+    if mode == "silent":
+        if exc:
+            msg = f"• <b>EVAL ERROR\n\n• CHAT:</b> <code>{get_display_name(event.chat)}</code> [<code>{event.chat_id}</code>]"
+            msg += f"\n\n∆ <b>CODE:</b>\n<code>{cmd}</code>\n\n∆ <b>ERROR:</b>\n<code>{exc}</code>"
+            log_chat = udB.get_key("LOG_CHANNEL")
+            if len(msg) > 4000:
+                with BytesIO(msg.encode()) as out_file:
+                    out_file.name = "Eval-Error.txt"
+                return await event.client.send_message(
+                    log_chat, f"`{cmd}`", file=out_file
+                )
+            await event.client.send_message(log_chat, msg, parse_mode="html")
+        return
+    tmt = tima * 1000
+    timef = time_formatter(tmt)
+    timeform = timef if not timef == "0s" else f"{tmt:.3f}ms"
+    final_output = "__►__ **EVAL** (__in {}__)\n```{}``` \n\n __►__ **OUTPUT**: \n```{}``` \n".format(
+        timeform,
+        cmd,
+        evaluation,
+    )
+    if len(final_output) > 4096:
+        final_output = evaluation
+        with BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.txt"
+            await event.client.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                thumb=ULTConfig.thumb,
+                allow_cache=False,
+                caption=f"```{cmd}```" if len(cmd) < 998 else None,
+                reply_to=reply_to_id,
+            )
+        return await xx.delete()
+    await xx.edit(final_output)
+
+
+def _stringify(text=None, *args, **kwargs):
+    if text:
+        u._ = text
+        text = _parse_eval(text)
+    return print(text, *args, **kwargs)
+
+
+async def aexec(code, event):
+    exec(
+        (
+            "async def __aexec(e, client): "
+            + "\n print = p = _stringify"
+            + "\n message = event = e"
+            + "\n u.r = reply = await event.get_reply_message()"
+            + "\n chat = event.chat_id"
+            + "\n u.lr = locals()"
+        )
+        + "".join(f"\n {l}" for l in code.split("\n"))
+    )
+
+    return await locals()["__aexec"](event, event.client)
+
+
+DUMMY_CPP = """#include <iostream>
+using namespace std;
+
+int main(){
+!code
+}
+"""
