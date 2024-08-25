0c425acad01494e7040aa754453c2cceca373c82
Just R <remixingmagelang@gmail.com>
2024-08-26 04:55:24 +0700

    update plugins/fluxai.py

--- plugins/fluxai.py
+++ plugins/fluxai.py
@@ -24,3 +24,23 @@ async def schellwithflux(args):
     return response.content
 
 @Client.on_message(filters.command(["fluxai"], prefix) & filters.me)
+async def imgfluxai_(client: Client, message: Message):
+    question = message.text.split(" ", 1)[1] if len(message.command) > 1 else None
+    if not question:
+        return await message.reply_text("mo apa kontol.")
+    try:
+        image_bytes = await schellwithflux(question)
+        if image_bytes is None:
+            return await message.reply_text("Failed to generate an image.")
+        pro = await message.reply_text("Generating image, please wait...")
+        with open("flux_gen.jpg", "wb") as f:
+            f.write(image_bytes)
+        ok = await pro.edit_text("Uploading image...")
+        await message.reply_photo("flux_gen.jpg", progress=progress, progress_args=(ok, time.time(), "Uploading image..."))
+        await ok.delete()
+        if os.path.exists("flux_gen.jpg"):
+            os.remove("flux_gen.jpg")
+    except Exception as e:
+        LOGS.error(str(e))
+        await message.edit_text(str(e))
+
