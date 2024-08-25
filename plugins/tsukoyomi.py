6f31183a73895e8777b5d93f795cbe40240f45d4
Just R <remixingmagelang@gmail.com>
2024-08-26 03:54:33 +0700

    update plugins/tsukoyomi.py

---
+++ plugins/tsukoyomi.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+from pyrogram import Client as ren
+from pyrogram import *
+from pyrogram import filters
+from pyrogram.errors import *
+from pyrogram.types import *
+from pyrogram.types import Message
+
+from utils.handler import *
+from utils.misc import plugins_help, prefix
