dba9a7ad770d75e2b1140480d148af8542d504e9
Just R <remixingmagelang@gmail.com>
2024-08-26 04:37:26 +0700

    update plugins/ceklimit.py

--- plugins/ceklimit.py
+++ plugins/ceklimit.py
@@ -12,7 +12,7 @@ from pyrogram.types import Message
 from utils.handler import *
 from utils.misc import plugins_help, prefix
 
-@Client.on_message(filters.command(["shell", "sh"], prefix) & filters.me)
+@Client.on_message(filters.command(["limit", "lmt"], prefix) & filters.me)
 
 async def spamban(client: Client, message: Message):
     await client.unblock_user("SpamBot")
