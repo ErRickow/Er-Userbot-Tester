e891a8ba2a03fcb073809431901354a45b4ce292
Just R <remixingmagelang@gmail.com>
2024-08-26 04:24:40 +0700

    update plugins/ceklimit.py

---
+++ plugins/ceklimit.py
@@ -0,0 +1,13 @@
+import asyncio
+import os
+
+from pyrogram import Client
+from pyrogram import Client as ren
+from pyrogram import *
+from pyrogram import filters
+from pyrogram.raw import *
+from pyrogram.types import *
+from pyrogram.types import Message
+
+from utils.handler import *
+from utils.misc import plugins_help, prefix
