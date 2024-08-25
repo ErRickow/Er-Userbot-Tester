8e7f92835163b7cd4cd36bb163dcb60829aa9da3
Just R <remixingmagelang@gmail.com>
2024-08-26 04:27:58 +0700

    update plugins/ceklimit.py

--- plugins/ceklimit.py
+++ plugins/ceklimit.py
@@ -19,3 +19,18 @@ from utils.misc import plugins_help, prefix
     & ~filters.forwarded
 )
 
+async def spamban(client: Client, message: Message):
+    await client.unblock_user("SpamBot")
+    response = await client.invoke(
+        raw.functions.messages.StartBot(
+            bot=await client.resolve_peer("SpamBot"),
+            peer=await client.resolve_peer("SpamBot"),
+            random_id=client.rnd_id(),
+            start_param="start",
+        )
+    )
+    wait_msg = await message.reply_text("`Processing . . .`")
+    await asyncio.sleep(1)
+    spambot_msg = response.updates[1].message.id + 1
+    status = await client.get_messages(chat_id="SpamBot", message_ids=spambot_msg)
+    await wait_msg.edit_text(f"~ {status.text}")
