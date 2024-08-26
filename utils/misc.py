0e7779e635eee3404c674841dbf5051e7ee74ed5
Just R <remixingmagelang@gmail.com>
2024-08-26 14:10:02 +0700

    update utils/misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -34,6 +34,7 @@ app = {}
 
 python_version = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
 
+asupan_username = db.get("core.main", "ASUPAN_USERNAME", " ")
 emopong = db.get("core.main", "emopong", " ")
 alive_logo = db.get("core.main", "alive_logo", " ")
 alive_text = db.get("core.main", "alive_text", " ")
