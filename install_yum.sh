27153f39f96f4f4e7b4dd17c6a4697adf461c3d0
Just R <remixingmagelang@gmail.com>
2024-08-27 03:30:52 +0700

    update install_yum.sh

--- install_yum.sh
+++ install_yum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if command -v termux-setup-storage; then
-  echo For termux, please use https://raw.githubusercontent.com/The-MoonTg-project/Moon-Userbot/main/termux-install.sh
+  echo For termux, please use https://raw.githubusercontent.com/ErRickow/Er-Userbot-Tester/main/termux-install.sh
   exit 1
 fi
 
