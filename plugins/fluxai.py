149dd1f00f55667465f7ff7e5fb2059a71876837
Just R <remixingmagelang@gmail.com>
2024-08-26 10:59:33 +0700

    update plugins/fluxai.py

--- plugins/fluxai.py
+++ plugins/fluxai.py
@@ -20,7 +20,7 @@ async def schellwithflux(args):
     response = requests.post(API_URL, json=payload)
     if response.status_code != 200:
         message.reply(f"Error status {response.status_code}")
-        return None
+        #return None
     return response.content
 
 @Client.on_message(filters.command(["fluxai"], prefix) & filters.me)
