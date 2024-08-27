55c4e0ab1cf422683dadc5cf73134e8f57f50bbb
Just R <remixingmagelang@gmail.com>
2024-08-28 02:47:47 +0700

    update plugins/ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -6,7 +6,6 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from utils.anu import time_formatter
-from erbanget import parse_mode
 from utils.misc import plugins_help, prefix, emopong
 
 StartTime = time.time()
