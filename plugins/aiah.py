6da226d5aae8cc36e6fe5694223f0980b1f30c56
Just R <remixingmagelang@gmail.com>
2024-09-08 02:24:58 +0700

    update plugins/aiah.py

---
+++ plugins/aiah.py
@@ -0,0 +1,7 @@
+import requests
+from pyrogram import *
+from pyrogram import Client, filters
+from pyrogram.types import *
+
+from utils.misc import plugins_help, prefix
+from utils.anu import format_exc
