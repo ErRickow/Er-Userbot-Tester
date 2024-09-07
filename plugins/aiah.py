1148eac79e06c813fb405108702af35d68a53b8f
Just R <remixingmagelang@gmail.com>
2024-09-08 02:25:18 +0700

    update plugins/aiah.py

--- plugins/aiah.py
+++ plugins/aiah.py
@@ -5,3 +5,20 @@ from pyrogram.types import *
 
 from utils.misc import plugins_help, prefix
 from utils.anu import format_exc
+
+async def mistraai(messagestr):
+    url = "https://randydev-ryuzaki-api.hf.space/api/v1/akeno/mistralai"
+    payload = {"args": messagestr}
+    response = requests.post(url, json=payload)
+    if response.status_code != 200:
+        return None
+    return response.json()
+
+async def chatgptold(messagestr):
+    url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/chatgpt-old"
+    payload = {"query": messagestr}
+    response = requests.post(url, json=payload)
+    if response.status_code != 200:
+        return None
+    return response.json()
+    
