e4f667c07ef41f3241a87ebdc5e68fd9d5fade13
Just R <remixingmagelang@gmail.com>
2024-09-10 01:05:58 +0700

    Delete the file at 'plugins/gatau.py'

--- plugins/gatau.py
+++
@@ -1,5 +0,0 @@
-payload = {"args": "Make body masterpiece potrait", "is_flux_dev": True}
-
-response = requests.post("https://akeno.randydev.my.id//akeno/fluxai?api_key=y0y0y03rq", json=payload).json()
-
-await message.reply_photo(response["randydev"]["output_url"], caption=response["caption"])
